7dc4ed754d1b19f403eee3c335c6fe3617836b6f
Alk0gol3nok <65295678+Alk0gol3nok@users.noreply.github.com>
2021-11-15 07:19:56 +0300

    Add files via upload

---
+++ SportForm.Designer.cs
@@ -0,0 +1,334 @@
+﻿
+namespace Firts_Forms
+{
+    partial class SportForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mainPannel = new System.Windows.Forms.Label();
+            this.mainPannel2 = new System.Windows.Forms.Panel();
+            this.nazad_reg_lb = new System.Windows.Forms.Label();
+            this.nazad_lg_lb = new System.Windows.Forms.Label();
+            this.tech_button = new System.Windows.Forms.Button();
+            this.kab_button = new System.Windows.Forms.Button();
+            this.kab_text = new System.Windows.Forms.Label();
+            this.review_button = new System.Windows.Forms.Button();
+            this.news_button = new System.Windows.Forms.Button();
+            this.enduro_button = new System.Windows.Forms.Button();
+            this.moto_eqip_button = new System.Windows.Forms.Button();
+            this.sport_button = new System.Windows.Forms.Button();
+            this.User_checker_field = new System.Windows.Forms.TextBox();
+            this.loginField = new System.Windows.Forms.TextBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.closeButton = new System.Windows.Forms.Label();
+            this.kab_png = new System.Windows.Forms.PictureBox();
+            this.nazadRegister_img = new System.Windows.Forms.PictureBox();
+            this.nazadLogin_img = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.mainPannel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.kab_png)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nazadRegister_img)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nazadLogin_img)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // mainPannel
+            // 
+            this.mainPannel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(19)))), ((int)(((byte)(81)))), ((int)(((byte)(172)))));
+            this.mainPannel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mainPannel.Font = new System.Drawing.Font("Comic Sans MS", 32F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.mainPannel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(218)))), ((int)(((byte)(211)))), ((int)(((byte)(219)))));
+            this.mainPannel.Location = new System.Drawing.Point(0, 0);
+            this.mainPannel.Name = "mainPannel";
+            this.mainPannel.Size = new System.Drawing.Size(1176, 80);
+            this.mainPannel.TabIndex = 0;
+            this.mainPannel.Text = "Главный экран";
+            this.mainPannel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // mainPannel2
+            // 
+            this.mainPannel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(75)))), ((int)(((byte)(158)))));
+            this.mainPannel2.Controls.Add(this.nazad_reg_lb);
+            this.mainPannel2.Controls.Add(this.nazad_lg_lb);
+            this.mainPannel2.Controls.Add(this.tech_button);
+            this.mainPannel2.Controls.Add(this.kab_button);
+            this.mainPannel2.Controls.Add(this.kab_png);
+            this.mainPannel2.Controls.Add(this.kab_text);
+            this.mainPannel2.Controls.Add(this.nazadRegister_img);
+            this.mainPannel2.Controls.Add(this.nazadLogin_img);
+            this.mainPannel2.Controls.Add(this.review_button);
+            this.mainPannel2.Controls.Add(this.news_button);
+            this.mainPannel2.Controls.Add(this.enduro_button);
+            this.mainPannel2.Controls.Add(this.moto_eqip_button);
+            this.mainPannel2.Controls.Add(this.sport_button);
+            this.mainPannel2.Controls.Add(this.User_checker_field);
+            this.mainPannel2.Controls.Add(this.loginField);
+            this.mainPannel2.Controls.Add(this.pictureBox1);
+            this.mainPannel2.Controls.Add(this.panel3);
+            this.mainPannel2.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.mainPannel2.Location = new System.Drawing.Point(-1, -23);
+            this.mainPannel2.Name = "mainPannel2";
+            this.mainPannel2.Size = new System.Drawing.Size(1176, 668);
+            this.mainPannel2.TabIndex = 2;
+            // 
+            // nazad_reg_lb
+            // 
+            this.nazad_reg_lb.AutoSize = true;
+            this.nazad_reg_lb.Location = new System.Drawing.Point(647, 647);
+            this.nazad_reg_lb.Name = "nazad_reg_lb";
+            this.nazad_reg_lb.Size = new System.Drawing.Size(136, 13);
+            this.nazad_reg_lb.TabIndex = 27;
+            this.nazad_reg_lb.Text = "Вернуться к регистрации";
+            // 
+            // nazad_lg_lb
+            // 
+            this.nazad_lg_lb.AutoSize = true;
+            this.nazad_lg_lb.Location = new System.Drawing.Point(789, 647);
+            this.nazad_lg_lb.Name = "nazad_lg_lb";
+            this.nazad_lg_lb.Size = new System.Drawing.Size(137, 13);
+            this.nazad_lg_lb.TabIndex = 26;
+            this.nazad_lg_lb.Text = "Вернуться к авторизации";
+            // 
+            // tech_button
+            // 
+            this.tech_button.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.tech_button.Location = new System.Drawing.Point(392, 183);
+            this.tech_button.Name = "tech_button";
+            this.tech_button.Size = new System.Drawing.Size(95, 23);
+            this.tech_button.TabIndex = 25;
+            this.tech_button.Text = "Тех.поддержка";
+            this.tech_button.UseVisualStyleBackColor = true;
+            // 
+            // kab_button
+            // 
+            this.kab_button.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.kab_button.FlatAppearance.BorderSize = 0;
+            this.kab_button.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.kab_button.Location = new System.Drawing.Point(1050, 158);
+            this.kab_button.Name = "kab_button";
+            this.kab_button.Size = new System.Drawing.Size(97, 23);
+            this.kab_button.TabIndex = 23;
+            this.kab_button.Text = "Перейти";
+            this.kab_button.UseVisualStyleBackColor = true;
+            // 
+            // kab_text
+            // 
+            this.kab_text.AutoSize = true;
+            this.kab_text.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.kab_text.Location = new System.Drawing.Point(1025, 135);
+            this.kab_text.Name = "kab_text";
+            this.kab_text.Size = new System.Drawing.Size(148, 20);
+            this.kab_text.TabIndex = 21;
+            this.kab_text.Text = "Личный кабинет";
+            // 
+            // review_button
+            // 
+            this.review_button.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.review_button.Location = new System.Drawing.Point(300, 183);
+            this.review_button.Name = "review_button";
+            this.review_button.Size = new System.Drawing.Size(75, 23);
+            this.review_button.TabIndex = 16;
+            this.review_button.Text = "Отзывы";
+            this.review_button.UseVisualStyleBackColor = true;
+            // 
+            // news_button
+            // 
+            this.news_button.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.news_button.Location = new System.Drawing.Point(506, 183);
+            this.news_button.Name = "news_button";
+            this.news_button.Size = new System.Drawing.Size(75, 23);
+            this.news_button.TabIndex = 15;
+            this.news_button.Text = "Новости";
+            this.news_button.UseVisualStyleBackColor = true;
+            // 
+            // enduro_button
+            // 
+            this.enduro_button.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.enduro_button.Location = new System.Drawing.Point(108, 183);
+            this.enduro_button.Name = "enduro_button";
+            this.enduro_button.Size = new System.Drawing.Size(75, 23);
+            this.enduro_button.TabIndex = 12;
+            this.enduro_button.Text = "Эндуро";
+            this.enduro_button.UseVisualStyleBackColor = true;
+            // 
+            // moto_eqip_button
+            // 
+            this.moto_eqip_button.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.moto_eqip_button.Location = new System.Drawing.Point(204, 183);
+            this.moto_eqip_button.Name = "moto_eqip_button";
+            this.moto_eqip_button.Size = new System.Drawing.Size(75, 23);
+            this.moto_eqip_button.TabIndex = 10;
+            this.moto_eqip_button.Text = "Мото-экип";
+            this.moto_eqip_button.UseVisualStyleBackColor = true;
+            // 
+            // sport_button
+            // 
+            this.sport_button.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.sport_button.Location = new System.Drawing.Point(18, 183);
+            this.sport_button.Name = "sport_button";
+            this.sport_button.Size = new System.Drawing.Size(75, 23);
+            this.sport_button.TabIndex = 8;
+            this.sport_button.Text = "Спортбайки";
+            this.sport_button.UseVisualStyleBackColor = true;
+            // 
+            // User_checker_field
+            // 
+            this.User_checker_field.Cursor = System.Windows.Forms.Cursors.No;
+            this.User_checker_field.Enabled = false;
+            this.User_checker_field.Font = new System.Drawing.Font("Times New Roman", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.User_checker_field.Location = new System.Drawing.Point(364, 96);
+            this.User_checker_field.Multiline = true;
+            this.User_checker_field.Name = "User_checker_field";
+            this.User_checker_field.ReadOnly = true;
+            this.User_checker_field.Size = new System.Drawing.Size(269, 51);
+            this.User_checker_field.TabIndex = 4;
+            // 
+            // loginField
+            // 
+            this.loginField.BackColor = System.Drawing.SystemColors.Control;
+            this.loginField.Enabled = false;
+            this.loginField.Font = new System.Drawing.Font("Times New Roman", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.loginField.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.loginField.Location = new System.Drawing.Point(80, 96);
+            this.loginField.Multiline = true;
+            this.loginField.Name = "loginField";
+            this.loginField.ReadOnly = true;
+            this.loginField.Size = new System.Drawing.Size(269, 51);
+            this.loginField.TabIndex = 3;
+            this.loginField.Text = "Вы вошли как:";
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(84)))), ((int)(((byte)(27)))), ((int)(((byte)(89)))));
+            this.panel3.Controls.Add(this.closeButton);
+            this.panel3.Controls.Add(this.mainPannel);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(1176, 80);
+            this.panel3.TabIndex = 0;
+            // 
+            // closeButton
+            // 
+            this.closeButton.AutoSize = true;
+            this.closeButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(19)))), ((int)(((byte)(81)))), ((int)(((byte)(172)))));
+            this.closeButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.closeButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.closeButton.ForeColor = System.Drawing.Color.White;
+            this.closeButton.Location = new System.Drawing.Point(1113, 0);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(24, 29);
+            this.closeButton.TabIndex = 1;
+            this.closeButton.Text = "x";
+            // 
+            // kab_png
+            // 
+            this.kab_png.Image = global::Firts_Forms.Properties.Resources.user;
+            this.kab_png.Location = new System.Drawing.Point(1063, 86);
+            this.kab_png.Name = "kab_png";
+            this.kab_png.Size = new System.Drawing.Size(74, 49);
+            this.kab_png.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.kab_png.TabIndex = 22;
+            this.kab_png.TabStop = false;
+            // 
+            // nazadRegister_img
+            // 
+            this.nazadRegister_img.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.nazadRegister_img.Image = global::Firts_Forms.Properties.Resources._return;
+            this.nazadRegister_img.Location = new System.Drawing.Point(689, 605);
+            this.nazadRegister_img.Name = "nazadRegister_img";
+            this.nazadRegister_img.Size = new System.Drawing.Size(45, 39);
+            this.nazadRegister_img.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.nazadRegister_img.TabIndex = 20;
+            this.nazadRegister_img.TabStop = false;
+            // 
+            // nazadLogin_img
+            // 
+            this.nazadLogin_img.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.nazadLogin_img.Image = global::Firts_Forms.Properties.Resources._return;
+            this.nazadLogin_img.Location = new System.Drawing.Point(832, 605);
+            this.nazadLogin_img.Name = "nazadLogin_img";
+            this.nazadLogin_img.Size = new System.Drawing.Size(44, 39);
+            this.nazadLogin_img.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.nazadLogin_img.TabIndex = 19;
+            this.nazadLogin_img.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Firts_Forms.Properties.Resources.computer;
+            this.pictureBox1.Location = new System.Drawing.Point(10, 83);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(64, 64);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // SportForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1173, 645);
+            this.Controls.Add(this.mainPannel2);
+            this.Name = "SportForm";
+            this.Text = "SportForm";
+            this.mainPannel2.ResumeLayout(false);
+            this.mainPannel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.kab_png)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nazadRegister_img)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nazadLogin_img)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label mainPannel;
+        private System.Windows.Forms.Panel mainPannel2;
+        private System.Windows.Forms.Label nazad_reg_lb;
+        private System.Windows.Forms.Label nazad_lg_lb;
+        private System.Windows.Forms.Button tech_button;
+        private System.Windows.Forms.Button kab_button;
+        private System.Windows.Forms.PictureBox kab_png;
+        private System.Windows.Forms.Label kab_text;
+        private System.Windows.Forms.PictureBox nazadRegister_img;
+        private System.Windows.Forms.PictureBox nazadLogin_img;
+        private System.Windows.Forms.Button review_button;
+        private System.Windows.Forms.Button news_button;
+        private System.Windows.Forms.Button enduro_button;
+        private System.Windows.Forms.Button moto_eqip_button;
+        private System.Windows.Forms.Button sport_button;
+        private System.Windows.Forms.TextBox User_checker_field;
+        private System.Windows.Forms.TextBox loginField;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label closeButton;
+    }
+}
